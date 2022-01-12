fe00567e0f373d674428678bbc04c62730daf22c
Celso Junior <cjunior.sfs@gmail.com>
2022-01-11 22:53:34 -0300

    correcao apos instalar dependencias do firebase

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,18 @@
-const path = require('path');
-
 module.exports = {
-    entry: './assets/scripts/src/index.ts',
-    resolve: {
-        extensions: ['.ts', '.js'],
-    },
-    module: {
-        rules: [{
-            test: /\.ts$/,
-            use: 'ts-loader',
-            exclude: /node_modules/,
-        }],
-    },
-    mode: 'development',
-    output: {
-        filename: 'bundle.js',
-    },
-    devServer: {
-        static: {
-            directory: path.join(__dirname),
-        },
-        port: 3000,
-    }
+  entry: "./public/assets/scripts/src/index.ts",
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  output: {
+    filename: "bundle.js",
+  },
 };
