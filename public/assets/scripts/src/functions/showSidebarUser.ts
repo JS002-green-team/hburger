0c5d2858887f154248c886e7834566c5c2a6b09a
Celso Junior <cjunior.sfs@gmail.com>
2022-01-17 20:05:23 -0300

    correcao show side bar

--- public/assets/scripts/src/functions/showSidebarUser.ts
+++ public/assets/scripts/src/functions/showSidebarUser.ts
@@ -1,8 +1,11 @@
 import iniciaModal from "./acionaModal";
 
-export default function showSidebarUser(){
-    const avatar = document.querySelector("#avatar") as HTMLImageElement;
+export default function showSidebarUser() {
+  const avatar = document.querySelector("#avatar") as HTMLImageElement;
+
+  if (avatar) {
     avatar.addEventListener("click", (e) => {
-        iniciaModal("user-modal");
-    })
+      iniciaModal("user-modal");
+    });
+  }
 }
