bef657034dfcffdeb9171529547a6e9959f58dfb
Celso Junior <cjunior.sfs@gmail.com>
2022-01-17 20:55:34 -0300

    pagina esqueci minha senha desenvolvida

--- public/assets/scripts/src/index.ts
+++ public/assets/scripts/src/index.ts
@@ -2,6 +2,7 @@ import "./firebase";
 import "./credicard";
 import "./auth-register";
 import "./auth-current";
+import "./auth-forget";
 import showSidebarUser from "./functions/showSidebarUser";
 
 showSidebarUser();
